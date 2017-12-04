5a0f2df034890c0f381845e5c921fe3c23ef2df0
Olaolu Olawuyi <whiskydee@gmail.com>
2017-12-04 09:47:08 +0100

    final commit

--- dist/js/viewport.js
+++ dist/js/viewport.js
@@ -4,24 +4,24 @@
   var width = window.innerWidth
   var height = window.innerHeight
   var text = width + ' x ' + height
-  
+
   document.documentElement.appendChild(indicator)
-  indicator.id = '__scrrenSizeIndicator'
+  indicator.id = 'ScreenSizeIndicator'
   indicator.style.zIndex = '99999999999'
   indicator.innerText = text
   indicator.style.position = 'fixed'
-  indicator.style.background = 'rgba(0, 0, 0, .95)'
+  indicator.style.background = 'rgba(0, 0, 0, .8)'
   indicator.style.borderRadius = '5px'
   indicator.style.color = '#f9f9f9'
   indicator.style.padding = '5px 10px'
   indicator.style.fontSize = '16px'
   indicator.style.bottom = '10px'
   indicator.style.right = '10px'
-  
-  window.addEventListener('resize', function() {
+
+  window.onresize = function() {
     var newwidth = window.innerWidth;
     var newheight = window.innerHeight;
     var newText = newwidth + ' x ' + newheight;
     indicator.innerText = newText
-  })
+  }
 }(window)
